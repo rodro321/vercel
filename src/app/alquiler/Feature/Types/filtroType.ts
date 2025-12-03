0b2d5c9d92843bff6560d755932a81f146c25f80
Rodrigo Alejandro Choque Bautista <rodrigochoquebautista000@gmail.com>
2025-12-02 22:54:48 -0400

    solucion filtros deployed

--- src/app/alquiler/Feature/Types/filtroType.ts
+++ src/app/alquiler/Feature/Types/filtroType.ts
@@ -106,9 +106,3 @@ export type UsuarioDetalle = {
 };
 
 // En filtroType.ts sigue UsuarioResumen
-
-/**export interface ConteosFiltros {
-  ciudades: Record<string, number>;       
-  especialidades: Record<string, number>; 
-  disponibilidad: Record<string, number>; 
-}*/
